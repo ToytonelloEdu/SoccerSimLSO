c31c564334b6cb4d82a4ff4a9dbd79cd92ec680b
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-02-22 10:40:24 +0100

    gameLogic experiments

--- zSandbox/gameLogic.c
+++ zSandbox/gameLogic.c
@@ -13,7 +13,26 @@
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 
+#include "gameLogicStructs.h"
+
+ball S = 1;
+
+void foo()
+{
+    wait(S);
+
+    //random selection:
+    //-shoot
+    //-dribbling
+    //-infortuns
+
+    signal(S);
+}
+
 int main()
 {
+    struct referee Ref;
+    Ref.teamA.membersFD[0] = 1;
+    Ref.teamB.membersFD[0] = 2;
     return 0;
 }
