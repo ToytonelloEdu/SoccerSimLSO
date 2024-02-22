06d5eaeb6b46d41b62b0db6569e4b5994ed99f47
ForxDer <fortunatoderrico11@gmail.com>
2024-02-22 12:31:36 +0100

    GameLogic advancements.

--- zSandbox/gameLogic.c
+++ zSandbox/gameLogic.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -16,23 +17,48 @@
 #include "gameLogicStructs.h"
 
 ball S = 1;
+struct referee Ref;
+enum actions {shot, injury, dribbling};
 
-void foo()
-{
-    wait(S);
+    void selectAction(struct player* player)
+    { 
+        wait(S);
+        sleep(2);
 
-    //random selection:
-    //-shoot
-    //-dribbling
-    //-infortuns
+        int result = rand() % 3;
+        
+        if (result == "shot" ) { shot(player); }
+        if (result == "injury" ) { injury(player); }
+        if (result == "dribbling" ) { dribbling(player); }
 
-    signal(S);
-}
+        signal(S);
+    }
 
-int main()
-{
-    struct referee Ref;
-    Ref.teamA.membersFD[0] = 1;
-    Ref.teamB.membersFD[0] = 2;
-    return 0;
-}
+    void shot(struct player* player)
+    {
+        int result = rand() % 2;    //0 GOAL -- 1 NO GOAL
+
+        if (result == 0) 
+        { goal(&Ref, &player); } 
+        else
+        { addShotFailed(&(Ref.stats)); }
+    }
+
+    void injury(struct player* player)
+    {
+
+    }
+
+    void dribbling(struct player* player)
+    {
+        int result = rand() % 2;    //0 SUCCESS -- 1 FAILED
+
+        addDribbling(&(Ref.stats));
+
+        if (result == 0) { shot(&player); }
+    }
+
+    int main()
+    {
+        return 0;
+    }
--- zSandbox/gameLogicStructs.h
+++ zSandbox/gameLogicStructs.h
@@ -1,37 +1,70 @@
+
 #ifndef GLSTRUCT
 #define GLSTRUCT
 
 #include <pthread.h>
 typedef struct referee;
 typedef struct team;
+typedef struct player;
+typedef struct stats;
 typedef int ball;
 
-void wait(ball mutex)
-{
-    while(mutex <= 0);
+    void wait(ball mutex)
+    {
+        while(mutex <= 0);
+        mutex--;
+    }
 
-    mutex--;
-}
+    void signal(ball mutex)
+    {
+        mutex++;
+    }
 
-void signal(ball mutex)
-{
-    mutex++;
-}
+    struct player
+    {
+        int playerFD;
+        char* name;
+        char shirtNumber; 
+        pthread_t playerTID;           //id for each thread managing of one the players/clients
+    };
 
-struct team //struct modeling a football team of 5 players
-{
-    int membersFD[5]; //file descriptors of sockets for each player/client
-    pthread_t membersTID[5]; //id for each thread managing of one the players/clients
+    struct team                        //struct modeling a football team of 5 players
+    {
+        char* teamName;
+        struct player members[5];      //file descriptors of sockets for each player/client
+        struct player* captain;         
+    };
+
+    void initTeam(struct team team)
+    {
+        team.captain = &(team.members[0]);
+    }
+
+    struct stats                       //struct modeling the stats of the game
+    {
+        int numberGoal;
+        int shotFailed;
+        int numberDribbling;
+    };    
+
+    struct referee                     //struct modeling the referee of the game
+    {
+        struct team teamA; 
+        struct team teamB;
+        struct stats stats;
+        int logFD;                     //file descriptor of the game log file
+        int time;
+    };
+
+    void goal(struct referee* referee, struct player* player)
+    {
+        referee -> stats.numberGoal++;
+        int FD = referee -> logFD;
 
 
-};
-
-struct referee //struct modeling the referee of the game
-{
-    struct team teamA; 
-    struct team teamB;
-    int logFD; //file descriptor of the game log file
-
-};
+    }
+    
+    void addDribbling(struct stats* stats) { stats -> numberDribbling++; }
+    void addShotFailed(struct stats* stats) { stats -> shotFailed++; }
 
 #endif
