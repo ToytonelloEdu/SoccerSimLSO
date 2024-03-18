f501115bb7e5d626df22d40c2250891d347c282d
ToytonelloEdu <istruzione.a.ascione@gmail.com>
2024-03-19 00:15:39 +0100

    Added several structs and functions for Probability of Actions and their success, plus refactoring for clean code

--- lib/gameLogicFuncts.h
+++ lib/gameLogicFuncts.h
@@ -11,14 +11,19 @@
 #include "clientManagement.h"
 #include "serverManagement.h"
 
-#define DURATION 10
-#define INJ_TIME_MOD 6
+#define DURATION 90
+#define ACT_INTER 5
+#define ACT_COOLDOWN 2
+#define INJ_TIME_MOD 25
 #define INJ_TIME_BASE 5
 
-void dribbling(struct referee* Ref, struct player* player, char* msg);
-void shot(struct referee* Ref, struct player* player, char* msg);
-void goal(struct referee* Ref, struct player* player, char* msg);
-void injury(struct referee* Ref, struct player* player, char* msg);
+struct actsProb;
+struct resProb;
+
+void Dribbling(struct referee* Ref, struct player* player, char* msg, struct resProb resP);
+void Shot(struct referee* Ref, struct player* player, char* msg, struct resProb resP);
+void Goal(struct referee* Ref, struct player* player, char* msg);
+void Injury(struct referee* Ref, struct player* player, char* msg);
 void addShotFailed(struct stats* stats);
 void addDribbling(struct stats* stats);
 
@@ -33,75 +38,136 @@ typedef int ball;
 
     void signal(ball* mutex)
     {
+        if(*mutex != 0) {*mutex = 0;} //per provare a correggere errore quando entrano 2 thread nel semaforo??
         (*mutex)++;
     }
 
+    /* 
     void delay(int num_of_seconds)
     {
         int milliseconds = 1000 * num_of_seconds;
         clock_t start = clock();
         while(clock() < start + milliseconds){}
+    } 
+    */
+
+    int valInBetween(int bottom, int val, int top)
+    {
+        if(val > bottom && val <= top) return 1;
+
+        return 0;
     }
 
-    void dribbling(struct referee* Ref, struct player* player, char* msg)
+    enum actions {eShot, eDribble, eInjury};
+
+    struct actsProb
     {
-        int result = rand() % 2;    //0 SUCCESS -- 1 FAILED
+        int ShotProb;
+        int DribbleProb;
+        int InjuryProb;
+        int Denom;
+    };
 
-        addDribbling(&(Ref -> stats));
-        strcat(msg, "\tDribbling");
+    struct actsProb defaultProbs = {40, 50, 10, 100};
 
-        if (result == 0) 
-        { strcat(msg, "\n"); shot(Ref, player, msg); }
+    enum actions getRandomAction(struct actsProb actP)
+    {
+        int bottom, top, val = (rand() % actP.Denom) + 1;
+        
+        bottom = 0; top = actP.ShotProb;
+        if(valInBetween(bottom, val, top)) return eShot;
+
+        bottom += actP.ShotProb; top += actP.DribbleProb;
+        if(valInBetween(bottom, val, top)) return eDribble;
+
+        bottom += actP.DribbleProb; top += actP.InjuryProb;
+        if(valInBetween(bottom, val, top)) return eInjury;
+
+        return -1;
+    }
+
+    enum actResult {failure, success};
+
+    struct resProb
+    {
+        int Numer;
+        int Denom;
+    };
+
+    enum actResult getActionResult(struct resProb resP)
+    {
+        int val = (rand() % resP.Denom) + 1;
+
+        if(valInBetween(0, val, resP.Numer)) return success;
+
+        return failure; 
+
+    }
+
+    struct resProb defDribbleProb = {3,5};
+    struct resProb defShotProb = {1,3};
+
+    void Dribbling(struct referee* Ref, struct player* player, char* msg, struct resProb resP)
+    {
+        enum actResult result = getActionResult(resP);
+        struct resProb shotRP = {2,5};
+
+        addDribbling(&(Ref -> stats)); strcat(msg, "\tDribbling");
+
+        if (result == success) 
+        { strcat(msg, "\n"); Shot(Ref, player, msg, shotRP);}
         else 
         {
             strcat(msg, " failed\n");
-            printf("%s", msg);
-            sendMSGtoAllClients(*Ref, msg);
-            writeLog(Ref->pathLogLib, msg);
+            sendMSGtoAllOutputs(*Ref, msg);
         }
     }
 
-    void shot(struct referee* Ref, struct player* player, char* msg)
+    void Shot(struct referee* Ref, struct player* player, char* msg, struct resProb resP)
     {
-        int result = rand() % 2;    //0 GOAL -- 1 NO GOAL
+        enum actResult result = getActionResult(resP);
 
         strcat(msg, "\tShot");
 
-        if (result == 0) 
-            { strcat(msg, "\n"); goal(Ref, player, msg); } 
+        if (result == success) 
+            { strcat(msg, "\n"); Goal(Ref, player, msg); } 
         else
-        { 
-            addShotFailed(&(Ref -> stats)); 
-            strcat(msg, " failed\n");
-            printf("%s", msg);
-            sendMSGtoAllClients(*Ref, msg);
-            writeLog(Ref->pathLogLib, msg);
+        {
+            addShotFailed(&(Ref -> stats));
+            strcat(msg, " failed\n"); sendMSGtoAllOutputs(*Ref, msg);
         }
     }
 
-    void goal(struct referee* Ref, struct player* player, char* msg)
+    void Goal(struct referee* Ref, struct player* player, char* msg)
     {
         if(player->team == 'A') {Ref->stats.numberGoalA++;}
         else if(player->team == 'B') {Ref->stats.numberGoalB++;}
 
         sprintf(msg, "%s\tGOAL: %d-%d\n", msg, Ref->stats.numberGoalA, Ref->stats.numberGoalB);
-        printf("%s", msg);
-        sendMSGtoAllClients(*Ref, msg);
-        
-        writeLog(Ref->pathLogLib, msg);
+        sendMSGtoAllOutputs(*Ref, msg);
     }
 
-    void injury(struct referee* Ref, struct player* player, char* msg)
+    int getInjuryTime()
     {
-        int mins = rand() % INJ_TIME_MOD + INJ_TIME_BASE;
-        player->resumePlay = Ref->time + mins;
-        if(player->resumePlay <= DURATION){
-            sprintf(msg, "%s\tInjured until min. %d (for %d minutes)\n", msg, player->resumePlay, mins);
-        } else {
-            sprintf(msg, "%s\tInjured until end of the game.\n", msg);
-        }
-        printf("%s", msg); sendMSGtoAllClients(*Ref, msg);
-        writeLog(Ref->pathLogLib, msg);
+        return (rand() % INJ_TIME_MOD) + INJ_TIME_BASE;
+    }
+
+    void getInjuryMSG(char* msg, struct player* player, int mins)
+    {
+        if(player->resumePlay <= DURATION)
+            {sprintf(msg, "%s\tInjured until min. %d (for %d minutes)\n", msg, player->resumePlay, mins);} 
+        else 
+            {sprintf(msg, "%s\tInjured until end of the game.\n", msg);}
+    }
+
+    void Injury(struct referee* Ref, struct player* player, char* msg)
+    {
+        int mins = getInjuryTime();
+
+        player->resumePlay = (Ref->time) + mins; 
+
+        getInjuryMSG(msg, player, mins);
+        sendMSGtoAllOutputs(*Ref, msg);
     }
     
     void addDribbling(struct stats* stats) { stats -> numberDribbling++; }
